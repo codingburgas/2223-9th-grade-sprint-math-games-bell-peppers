0922f3581e13b6a9d22dee8c7d39f828fab0813d
VDChavdarova21 <VDChavdarova21@codingburgas.bg>
2023-03-17 20:06:26 +0200

    Add quit button. Close #12

--- do not stop talking or boom/_build/main.cpp
+++ do not stop talking or boom/_build/main.cpp
@@ -10,9 +10,13 @@ int main() {
 
 
 		BeginDrawing();
-		
+
 		DrawTexture(menu, 0, 0, WHITE);
-		
+
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMouseX() >= 94 && GetMouseX() <= 209 && GetMouseY() >= 745 && GetMouseY() <= 800) {
+			return 0;
+		}
+
 		EndDrawing();
 	}
 	CloseWindow();
