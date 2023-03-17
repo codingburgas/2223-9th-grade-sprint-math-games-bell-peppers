d2a9c24d44db3f929541833ed8d8d2b843e2af8c
VDChavdarova21 <VDChavdarova21@codingburgas.bg>
2023-03-17 20:04:28 +0200

    Add texture for main menu. Close #11

--- do not stop talking or boom/_build/main.cpp
+++ do not stop talking or boom/_build/main.cpp
@@ -2,5 +2,18 @@
 
 int main() {
 
-	
+	InitWindow(1600, 1000, "Bombs");
+
+	Texture2D menu = LoadTexture("../resources/menu.png");
+
+	while (!WindowShouldClose()) {
+
+
+		BeginDrawing();
+		
+		DrawTexture(menu, 0, 0, WHITE);
+		
+		EndDrawing();
+	}
+	CloseWindow();
 }
