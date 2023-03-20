7a1106cb4915e040f2c17a7b8dc926853d8dbc04
VDChavdarova21 <VDChavdarova21@codingburgas.bg>
2023-03-20 19:51:25 +0200

    Add main menu. Close #5

--- do not stop talking or boom/_build/main.cpp
+++ do not stop talking or boom/_build/main.cpp
@@ -6,14 +6,21 @@ int main() {
 
 	Texture2D menu = LoadTexture("../resources/menu.png");
 
-	while (!WindowShouldClose()) {
+	SetWindowIcon(LoadImage("../resources/menu.png"));
 
+	while (!WindowShouldClose()) {
 
 		BeginDrawing();
 
 		DrawTexture(menu, 0, 0, WHITE);
 
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMouseX() >= 94 && GetMouseX() <= 209 && GetMouseY() >= 745 && GetMouseY() <= 800) {
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMouseX() >= 94 && GetMouseX() <= 219 && GetMouseY() >= 365 && GetMouseY() <= 430) {
+			UnloadTexture(menu);
+		}
+		else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMouseX() >= 94 && GetMouseX() <= 209 && GetMouseY() >= 550 && GetMouseY() <= 605) {
+			UnloadTexture(menu);
+		}
+		else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMouseX() >= 94 && GetMouseX() <= 209 && GetMouseY() >= 755 && GetMouseY() <= 810) {
 			return 0;
 		}
 
